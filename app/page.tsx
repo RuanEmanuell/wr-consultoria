34561c70d80cbb6e398a4b142d58d994dd88c498
RuanEmanuell <ruanvitorcom540@gmail.com>
2024-09-20 10:16:34 -0300

    (Version 1.0) Starting mobile menu function

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,63 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import "./globals.css";
 import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [navBarHeight, setNavbarHeight] = useState<number>(64);
+  const [mobileMenuVisible, setMobileMenuVisible] = useState<boolean>(false);
 
   useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if(window.scrollY > 0){
-        setNavbarHeight(0);
-      } else {
-        setNavbarHeight(64);
-      }
-    });
-  }, []);
-  
+    if (mobileMenuVisible) {
+      document.querySelector("body")!.style.overflow = "hidden";
+    } else {
+      document.querySelector("body")!.style.overflow = "auto";
+    }
+  }, [mobileMenuVisible]);
+
   return (
     <div className="h-fit">
-      <nav className="bg-[#003366] w-full h-16 flex flex-row justify-between transition-all ease-in-out duration-500" style={{height: navBarHeight}}>
-        <Image src="/logo.png" className="w-24 h-16 p-4" alt="" width={1000} height={1000} />
-        <div className="m-auto w-1/4 hidden md:flex justify-between px-4">
-          <a className="font-semibold text-white">Teste</a>
-          <a className="font-semibold text-white">Teste</a>
-          <a className="font-semibold text-white">Teste</a>
+      <div className="flex flex-col">
+        <nav
+          className="bg-[#003366] w-full flex flex-row justify-between transition-all ease-in-out duration-500">
+          <Image
+            src="/logo.png"
+            className="w-24 h-16 p-4"
+            alt="Logo"
+            width={1000}
+            height={1000}
+          />
+          <div className="m-auto w-1/2 max-w-xl hidden md:flex justify-between px-4">
+            <a className="font-semibold text-white" href="#screen-1">
+              Serviços
+            </a>
+            <a className="font-semibold text-white" href="#screen-2">
+              Sobre nós
+            </a>
+            <a className="font-semibold text-white" href="#screen-3">
+              Contato
+            </a>
+          </div>
+          <div className="flex md:hidden my-auto mx-4 w-8 h-6 flex-col" onClick={() => setMobileMenuVisible(true)}>
+            <section className="bg-white h-1/3 w-full"></section>
+            <section className="bg-white h-1/3 w-full my-1"></section>
+            <section className="bg-white h-1/3 w-full"></section>
+          </div>
+        </nav>
+        <main className="h-fit">
+          <div className="h-screen w-full" id="screen-0">
+            <p>a</p>
+          </div>
+          <div className="h-screen w-full bg-[#003366]" id="screen-1"></div>
+          <div className="h-screen w-full" id="screen-2"></div>
+          <div className="h-screen w-full bg-[#003366]" id="screen-3"></div>
+        </main>
+      </div>
+      {mobileMenuVisible &&
+        <div className="w-screen h-screen bg-black bg-opacity-40 absolute top-0" id="mobile-menu" onClick={() => setMobileMenuVisible(false)}>
+
         </div>
-        <div className="flex md:hidden my-auto mx-4 w-8 h-6 flex-col">
-          <section className="bg-white h-1/3 w-full"></section>
-          <section className="bg-white h-1/3 w-full my-1"></section>
-          <section className="bg-white h-1/3 w-full"></section>
-        </div>
-      </nav>
-      <main className="h-fit">
-        <div className="h-screen w-full screen-0">
-        </div>
-        <div className="h-screen w-full bg-[#003366] screen-1">
-        </div>
-      </main>
+      }
     </div>
   );
 }
