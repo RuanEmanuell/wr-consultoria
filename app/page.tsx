65242b93743544ac9231ac9bcdacbf40052512a1
Ruan Emanuell Alencar <113607857+RuanEmanuell@users.noreply.github.com>
2024-10-11 11:30:42 -0300

    Feat: Adding partnerships page

--- app/page.tsx
+++ app/page.tsx
@@ -69,20 +69,23 @@ export default function Home() {
           <div className="h-fit w-full bg-[#81a64e]" id="screen-0">
             <div className="max-w-9xl mx-auto">
               <h1 className="text-center text-white text-5xl xl:text-7xl font-bold py-10">WR Consultoria</h1>
-              <div className="flex flex-col xl:grid xl:grid-cols-2">
+              <div className="flex flex-col lg:grid lg:grid-cols-2">
                 <div className="max-w-5xl mx-auto">
                   <Image
                     src="/logo.png"
-                    className="w-48 md:w-64 my-8 mx-auto"
+                    className="w-48 xl:w-64 my-8 mx-auto"
                     alt="Logo"
                     width={10000}
                     height={10000}
                   />
-                  <h2 className="text-center text-white text-2xl 2xl:text-3xl font-bold py-2 mx-10">Somos uma empresa dedicada a trazer inovação e eficiência para o seu negócio. Nossa missão é ajudar empresas a alcançarem o máximo de produtividade, bem-estar e desempenho com soluções personalizadas e focadas nos resultados. Melhore os resultados da sua empresa agora, de onde você estiver. </h2>
+                  <h2 className="text-center text-white text-2xl lg:text-xl xl:text-2xl 2xl:text-3xl font-bold py-2 mx-10">Somos uma empresa dedicada a trazer inovação e eficiência para o seu negócio. Nossa missão é ajudar empresas a alcançarem o máximo de produtividade, bem-estar e desempenho com soluções personalizadas e focadas nos resultados. Melhore os resultados da sua empresa agora, de onde você estiver. </h2>
+                  <a className="bg-white rounded-full w-3/4 md:w-96 h-32 mx-auto mt-4 bg-[#81a64e] flex justify-center items-center cursor-pointer text-2xl text-[#81a64e] font-bold text-center  hover:bg-gray-600 hover:text-white transition-all" href="https://wa.me/551533185825?text=Quero%20saber%20mais%20sobre%20como%20a%20WR%20Consultoria%20pode%20me%20ajudar!" target="_blank">
+                    <h4 className="mx-auto px-5">Agende já sua reunião e conheça a WR</h4>
+                  </a>
                 </div>
                 <Image
                   src="/smartphone.png"
-                  className="w-128 xl:w-144 mt-4 mx-auto"
+                  className="w-128 md:w-144 mx-auto"
                   alt="Logo"
                   width={10000}
                   height={10000}
@@ -118,9 +121,37 @@ export default function Home() {
           <div className="h-fit w-full bg-[#81a64e] pb-4 xl:pb-10" id="screen-2">
             <div className="max-w-7xl mx-auto">
               <h1 className="text-center text-white text-5xl md:text-7xl font-bold py-10 mb-6">Sobre nós</h1>
-              <div className="flex flex-col xl:grid xl:grid-cols-2 mb-6 xl:mb-16">
+              <div className="flex flex-col lg:grid lg:grid-cols-2 mb-6 lg:mb-0">
                 <div className="flex flex-col">
-                  <div className="flex flex-col rounded w-4/5 h-72 md:w-96 xl:h-full my-auto mx-auto relative">
+                  <div className="flex flex-col rounded w-4/5 h-72 sm:w-96 lg:h-full my-auto mx-auto relative">
+                    <Image
+                      src="/washington_e_raquel.jpeg"
+                      className="w-full h-full mx-auto object-cover"
+                      alt="Logo"
+                      width={10000}
+                      height={10000}
+                    />
+                    <section className="flex flex-col bg-white w-full xl:w-full mx-auto pb-2">
+                      <figcaption className="text-center font-semibold text-2xl mt-2">
+                        Washington Alencar e Raquel Alencar
+                      </figcaption>
+                      <figcaption className="text-center text-gray-500 font-semibold text-lg mx-2">
+                        Cofundadores da WR Consultoria
+                      </figcaption>
+                    </section>
+                  </div>
+                </div>
+                <div className="mx-auto">
+                  <h2 className="text-center text-white text-xl xl:text-2xl font-bold py-5 lg:py-10 mx-10 lg:mx-auto lg:w-4/5 mt-24 lg:mt-0">Washington Alencar e Raquel Alencar são os cofundadores da WR Consultoria, uma empresa renomada na área de desenvolvimento organizacional e de pessoas. Com vasta experiência e conhecimento, eles se destacam como especialistas em transformar a performance e a produtividade das empresas através de pessoas bem capacitadas.
+                    A WR Consultoria, sob a liderança de Washington e Raquel, oferece soluções personalizadas que atendem às necessidades específicas de cada cliente. Eles são parceiros estratégicos para qualquer empresa que busca elevar seu nível de desempenho, promovendo um ambiente de trabalho mais eficiente e colaborativo.</h2>
+                </div>
+              </div>
+              <div className="flex flex-col-reverse lg:grid lg:grid-cols-2 mb-6 lg:mb-0">
+                <div className="mx-auto">
+                  <h2 className="text-center text-white text-xl xl:text-2xl font-bold py-5 lg:py-10 mx-10 lg:mx-auto lg:w-4/5 mt-24 lg:mt-0">Raquel Alencar atua como consultora e palestrante em diversas áreas da neurociência, com foco em gestão, neurobranding, neuromarketing e saúde corporativa. Além de desenvolver estratégias para otimizar processos e fortalecer marcas, Raquel oferece treinamento em mindfulness e inteligência emocional, promovendo o bem-estar e a saúde mental dos colaboradores. Sua abordagem combina ciência e práticas inovadoras para impulsionar a eficiência organizacional e o desenvolvimento humano nas empresas.</h2>
+                </div>
+                <div className="flex flex-col">
+                  <div className="flex flex-col rounded w-4/5 h-72 sm:w-96 lg:h-full my-auto mx-auto relative">
                     <Image
                       src="/raquel.jpeg"
                       className="w-full h-full mx-auto object-cover"
@@ -134,18 +165,10 @@ export default function Home() {
                     </section>
                   </div>
                 </div>
-                <div className="mx-auto">
-                  <h2 className="text-center text-white text-xl xl:text-2xl font-bold py-5 xl:py-10 mx-10 lg:mx-auto xl:w-4/5 mt-24 xl:mt-0">Raquel Alencar atua como consultora e palestrante em diversas áreas da neurociência, com foco em gestão, neurobranding, neuromarketing e saúde corporativa. Além de desenvolver estratégias para otimizar processos e fortalecer marcas, Raquel oferece treinamento em mindfulness e inteligência emocional, promovendo o bem-estar e a saúde mental dos colaboradores. Sua abordagem combina ciência e práticas inovadoras para impulsionar a eficiência organizacional e o desenvolvimento humano nas empresas.</h2>
-                </div>
               </div>
-              <div className="flex flex-col-reverse xl:grid xl:grid-cols-2 mb-6 xl:mb-0 pb-10">
-                <div className="mx-auto">
-                  <h2 className="text-center text-white text-xl xl:text-2xl font-bold py-5 xl:py-10 mx-10 lg:mx-auto xl:w-4/5 mt-20 xl:mt-0">
-                    Washington Alencar atua como consultor e palestrante com mais de 12 anos de experiência liderando equipes e conduzindo treinamentos. Especialista em gestão de pessoas, liderança e alta performance, sua trajetória é marcada por um compromisso contínuo com o desenvolvimento humano e organizacional. Washington capacita profissionais para alcançar seu máximo potencial, e entre suas experiências, destaca-se o curso “Space Experience” na NASA, focado em estratégias disruptivas e aprendizagem ativa.
-                  </h2>
-                </div>
+              <div className="flex flex-col lg:grid lg:grid-cols-2 mb-10">
                 <div className="flex flex-col">
-                  <div className="flex flex-col rounded w-4/5 h-72 md:w-96 xl:h-full my-auto mx-auto relative">
+                  <div className="flex flex-col rounded w-4/5 h-72 sm:w-96 lg:h-full my-auto mx-auto relative">
                     <Image
                       src="/washington.jpeg"
                       className="w-full h-full mx-auto object-cover"
@@ -163,24 +186,8 @@ export default function Home() {
                     </section>
                   </div>
                 </div>
-              </div>
-              <div className="flex flex-col xl:grid xl:grid-cols-2 mb-6 xl:mb-0">
-                <div className="flex flex-col">
-                  <div className="flex flex-col rounded w-4/5 h-128 md:w-128 my-auto mx-auto">
-                    <Image
-                      src="/washington_e_raquel.jpeg"
-                      className="w-full h-full mx-auto object-cover border-4 rounded"
-                      alt="Logo"
-                      width={10000}
-                      height={10000}
-                    />
-                  </div>
-                </div>
-                <div className="mx-auto flex items-center justify-center">
-                  <h2 className="text-center text-white text-xl xl:text-2xl font-bold py-5 mx-10 lg:mx-auto xl:w-4/5">
-                    Washington Alencar e Raquel Alencar são os cofundadores da WR Consultoria, uma empresa renomada na área de desenvolvimento organizacional e de pessoas. Com vasta experiência e conhecimento, eles se destacam como especialistas em transformar a performance e a produtividade das empresas através de pessoas bem capacitadas.
-                    A WR Consultoria, sob a liderança de Washington e Raquel, oferece soluções personalizadas que atendem às necessidades específicas de cada cliente. Eles são parceiros estratégicos para qualquer empresa que busca elevar seu nível de desempenho, promovendo um ambiente de trabalho mais eficiente e colaborativo.
-                  </h2>
+                <div className="mx-auto">
+                  <h2 className="text-center text-white text-xl xl:text-2xl font-bold py-5 lg:py-10 mx-10 lg:mx-auto lg:w-4/5 mt-24 lg:mt-0">Washington Alencar atua como consultor e palestrante com mais de 12 anos de experiência liderando equipes e conduzindo treinamentos. Especialista em gestão de pessoas, liderança e alta performance, sua trajetória é marcada por um compromisso contínuo com o desenvolvimento humano e organizacional. Washington capacita profissionais para alcançar seu máximo potencial, e entre suas experiências, destaca-se o curso “Space Experience” na NASA, focado em estratégias disruptivas e aprendizagem ativa.</h2>
                 </div>
               </div>
             </div>
@@ -230,7 +237,40 @@ export default function Home() {
               </a>
             </section>
           </div>
-
+          <div className="h-fit w-full bg-[#81a64e]" id="screen-5">
+            <div className="max-w-9xl mx-auto pb-10">
+              <h1 className="text-center text-white text-5xl xl:text-7xl font-bold py-10">Empresas parceiras</h1>
+              <section className="grid grid-cols-3 mx-auto max-w-5xl">
+                <div className="w-36 h-24 md:w-48 md:h-32 mx-auto flex justify-center items-center">
+                  <Image
+                    src="/educacao-adventista.png"
+                    className="w-2/3 h-full"
+                    alt="Logo"
+                    width={10000}
+                    height={10000}
+                  />
+                </div>
+                <div className="w-36 h-24 md:w-48 md:h-32 mx-auto flex justify-center items-center">
+                  <Image
+                    src="/international-school.png"
+                    className="w-2/3 h-full"
+                    alt="Logo"
+                    width={10000}
+                    height={10000}
+                  />
+                </div>
+                <div className="w-36 h-24 md:w-48 md:h-32 mx-auto flex justify-center items-center">
+                  <Image
+                    src="/viaje-brazil.png"
+                    className="w-2/3 h-full"
+                    alt="Logo"
+                    width={10000}
+                    height={10000}
+                  />
+                </div>
+              </section>
+            </div>
+          </div>
         </main>
       </div>
       {mobileMenuVisible &&
